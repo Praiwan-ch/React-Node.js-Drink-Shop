8812d0223e4191d7f464d0c2030bcdc3a1e217b6
Praiwan Channarong <63160256@go.buu.ac.th>
2022-10-07 16:31:59 +0700

    Edit main and css

--- views/src/components/Main.js
+++ views/src/components/Main.js
@@ -39,62 +39,60 @@ export default function Main(props){
 
 
     return (
-    
-        
-
         <div className='main' id='main'>
-                <div className='header'>
-                    <h4>"San-kai" Drink's Shop</h4>
-                    <div className='search'>
-                        <label htmlFor='searchInput'>
-                            <div id='searchBox'>
-                                <span id='searchIcon'><i className='bi bi-search'></i></span>
-                                <span id='searchIcon2'>|</span>
-                                <input id='searchInput' type='text' placeholder='ค้นหา...' autoComplete='off' 
-                                    // onInput={(event)=>{
-                                    //     searchMenu(event.target.value)
-                                    // }}
-                                ></input>
-                            </div>
-                        </label>
-                    </div>
+            <div className='header'>
+                <h4>"San-kai" Drink's Shop</h4>
+                <div className='search'>
+                    <label htmlFor='searchInput'>
+                        <div id='searchBox'>
+                            <span id='searchIcon'><i className='bi bi-search'></i></span>
+                            <span id='searchIcon2'>|</span>
+                            <input id='searchInput' type='text' placeholder='ค้นหา...' autoComplete='off' 
+                                // onInput={(event)=>{
+                                //     searchMenu(event.target.value)
+                                // }}
+                            ></input>
+                        </div>
+                    </label>
                 </div>
+            </div>
 
-                
-                <div className='nav'>
-                    <button className='btn' onClick={getMenu}>ALL</button>
-                    <button className='btn' onClick={getMenuHot}>HOT</button>
-                    <button className='btn' onClick={getMenuIced}>ICED</button>
-                    <button className='btn' onClick={getMenuFrappe}>FRAPPE</button>
-                </div>
+            
+            <div className='nav'>
+                <button className='btn' onClick={getMenu}>ALL</button>
+                <button className='btn' onClick={getMenuHot}>HOT</button>
+                <button className='btn' onClick={getMenuIced}>ICED</button>
+                <button className='btn' onClick={getMenuFrappe}>FRAPPE</button>
+            </div>
+            
+            <div className='menu'>
+                {
 
-                <div className='menu'>
-                    {
-
-                        menu.map((val)=>{
-                                return (
-                                    <div className='card'>
-                                    <div className='img'>
-                                        <img src={'./image/'+val.menu_image} alt={val.menu_image}></img>
-                                    </div>
-                                    <div className='label-item'>
-                                        <p>{val.menu_name}</p>
+                    menu.map((val)=>{
+                        return (
+                            <div className='card'>
+                                <div className='img'>
+                                    <img src={'./image/'+val.menu_image} alt={val.menu_image}></img>
+                                </div>
+                                <div className='label-item'>
+                                    <p>{val.menu_name}</p>
+                                    <div class="btn-group">
                                         <span>{val.menu_price} ฿</span>
+                                        <div className="btn-card" id={val.menu_id}  
+                                            onClick={(event)=>{
+                                            send_object(event.target.id)
+                                        }}></div>
                                     </div>
-                                    </div>
-                                )
-                        })
-                    }
-                </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
-
-    
-
-    
-
-
-        
-        
-    
     );
+
+    function send_object(val){
+        console.log(val)
+    }
 }
